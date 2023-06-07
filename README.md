102c53976282b4def2dde81fd5a5115b12708684
marek <marek@niedzwiedz.co>
2023-06-07 10:05:00 +0200

    Added private prompts

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ To use it, you have to:
     - prompt() - select new prompt;
     - exit() - exit;
     - use """ to start multiline input; you have to finish it as well with """.
+5. if you want to add private prompts just create a file 'private_prompts.py' with variable inside of it PRIVATE_PROMPTS
+    - e.g PRIVATE_PROMPTS = [
+        [ {'role':'user', 'content':'I want to chat with you.'} ],
+        ]
 
 
 I've been using it for a few weeks and find it helpful. I hope you will find it useful as well.
--- prompts.py
+++ prompts.py
@@ -1,3 +1,5 @@
+import os
+
 PROMPTS = [
     [ {'role':'user', 'content':'I want to chat with you.'} ],
     [ {'role':'user', 'content':'I will ask you a question about the subject that I want to learn about. Identify and share the most important 20% of learnings from this topic that will help me understand 80% of it.'} ],
@@ -6,3 +8,8 @@ PROMPTS = [
     [ {'role':'user', 'content':'I will ask you to help me create a report on the specific subject or topic. Research and create an in-depth report with a step-by-step guide that will help readers understand it.'} ],
     [ {'role':'user', 'content':'I will ask you about the skill i want to learn. Generate a 30 day plan that will help a beginner like me learn the skill from scratch.'} ],
 ]
+
+FILENAME_PRIVATE_PROMOTS = 'private_prompts.py'
+if os.path.isfile(FILENAME_PRIVATE_PROMOTS):
+    from private_prompts import PRIVATE_PROMPTS
+    PROMPTS += PRIVATE_PROMPTS
